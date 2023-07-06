91ded48865c4598de35e274acee35f2b61616cf6
Henrique Campos <henriquecampos@Henriques-MacBook-Pro.local>
2023-07-06 17:36:54 +0200

    reverse to no web

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  // reactStrictMode: true,
+  // output: 'export',
+  // distDir: 'build'
+  experimental: {
+    // reactRefresh: false,
+  },
 }
 
 module.exports = nextConfig
-
