093f5848021d027af1fac8b4d6242041b4f6956c
Brian Saavedra <blsaavedra@uc.cl>
2023-09-26 14:25:46 -0300

    fix(loader pdfs): centered the loader

--- components/premiumMaterial/Loader.js
+++ components/premiumMaterial/Loader.js
@@ -4,17 +4,19 @@ import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 
 const CircularProgressBar = ({ progress }) => {
   return (
-    <div style={{ width: '100px', height: '100px' }}>
-      <CircularProgressbar
-        value={progress}
-        text={`${progress}%`}
-        strokeWidth={5}
-        styles={buildStyles({
-          textColor: 'var(--green)', 
-          pathColor: 'var(--green)', // Customize progress bar color
-          trailColor: 'lightgray', // Customize trail color
-        })}
-      />
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '80vh', overflow: 'hidden' }}>
+      <div style={{ width: '100px', height: '100px' }}>
+        <CircularProgressbar
+          value={progress}
+          text={`${progress}%`}
+          strokeWidth={5}
+          styles={buildStyles({
+            textColor: 'var(--green)', 
+            pathColor: 'var(--green)', // Customize progress bar color
+            trailColor: 'lightgray', // Customize trail color
+          })}
+        />
+      </div>
     </div>
   );
 };
