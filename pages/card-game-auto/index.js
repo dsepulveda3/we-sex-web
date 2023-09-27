3a41a066d31ea8b9967b2d11772089ed00e536e8
diegosepulv <dsepulveda3@uc.cl>
2023-09-27 18:44:33 -0300

    feat(game): auto show

---
+++ pages/card-game-auto/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import CardAuto from '../../components/CardGame/cardAuto';
+
+
+export default function Home() {
+  
+  return (
+    <>
+    <Head>
+        <title>Juego de Cartas - WeSex</title>
+        <meta name="description" content="Guía Tantra + Sexo Anal - WeSex" /> 
+        <meta name="keywords" content="" /> 
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="/img/favicon/site.webmanifest"/>
+      </Head>
+
+      <main>
+        <CardAuto />
+      </main>
+    </>
+  )
+}
