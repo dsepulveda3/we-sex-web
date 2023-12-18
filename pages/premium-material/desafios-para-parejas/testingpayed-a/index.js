3cadc2da1f79ef4a4cc2c4a1b37085b76c1a71ea
diegosepulv <dsepulveda3@uc.cl>
2023-12-18 15:00:18 -0300

    feat(desafios): new url ab testing no free month

---
+++ pages/premium-material/desafios-para-parejas/testingpayed-a/index.js
@@ -0,0 +1,94 @@
+import Head from 'next/head'
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Layout from '../../../../components/general/Layout';
+import IntroDiscount from '../../../../components/premiumMaterial/desafiosParejas/introDiscount';
+import Explicacion2 from '../../../../components/premiumMaterial/desafiosParejas/explicacion2'
+import Explicacion from '../../../../components/premiumMaterial/explicacion'
+import ComoFunciona from '../../../../components/premiumMaterial/desafiosParejas/comoFunciona'
+import Dudas from '../../../../components/premiumMaterial/dudas'
+import PlansUniversal from '../../../../components/premiumMaterial/desafiosParejas/plansUniversal';
+import Opinions from '../../../../components/premiumMaterial/desafiosParejas/opinions';
+
+export default function TestingpayedA() {
+  const router = useRouter();
+  const [origin, setOrigin] = useState(null);
+
+  useEffect(() => {
+    if (router.isReady){
+      if (router.query.origin) {
+        setOrigin(router.query.origin);
+      }
+    }
+  }, [router.isReady]);
+
+  useEffect(() => {
+    // Check if window is defined (client-side)
+    if (typeof window !== 'undefined') {
+      // This code will only run on the client side
+        const currentRoute = window.location.pathname + window.location.search;
+
+        // Send the complete route to Freshpaint as a custom event
+        if(currentRoute === '/premium-material/desafios-para-parejas/testingpayed-a?origin=Argentina'){
+            freshpaint.track("Test payed A (Argentina) - PRICING ", {"Path": currentRoute});
+        } else if (currentRoute === '/premium-material/desafios-para-parejas/testingpayed-a?origin=Otro'){
+          freshpaint.track("Test payed A (Otro) - PRICING ", {"Path": currentRoute});
+        } else if (currentRoute === '/premium-material/desafios-para-parejas/testingpayed-a?origin=Chile'){
+          freshpaint.track("Test payed A (Chile) - PRICING ", {"Path": currentRoute});
+        }
+        
+    }
+  }, []);
+
+  
+  
+  return (
+    <>
+      <Head>
+        <title>Desafio para parejas - WeSex</title>
+        <meta name="description" content="Reavivar la llama con tu pareja - WeSex" /> 
+        <meta name="keywords" content="" /> 
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="/img/favicon/site.webmanifest"/>
+      </Head>
+
+      <main>
+        <Layout>
+        <IntroDiscount subtitle1_1='' subtitle1_2='Desafios sexuales' subtitle1_3='para hacer en pareja'/>
+        {/* <div style={{color: "black", textAlign: "center", fontSize: "3rem", fontWeight: "bold"}}>TESTING A</div> */}
+        <Explicacion2 />
+        <ComoFunciona />
+        <Dudas />
+        <Opinions />
+        <PlansUniversal 
+        discount=""
+        country={origin}
+        
+
+        price_mp_basic="20,000 ARS/pareja"
+        mp_basic="https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c9380848c7d076c018c7df8f0cb00e2" 
+        price_mp_estandar="32,000 ARS/pareja"
+        mp_estandar="https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c9380848c7d075a018c7df9bb4900f1"
+        price_mp_premium="44,000 ARS/pareja"
+        mp_premium="https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c9380848c7d0790018c7dfadfba00f2"
+
+        price_stripe_basic="30 USD/pareja"
+        stripe_basic="https://buy.stripe.com/9AQeXCaVXbH71RSaFT"
+        price_stripe_estandar="38 USD/pareja"
+        stripe_estandar="https://buy.stripe.com/bIY7va0hjdPf9kk4hw"
+        price_stripe_premium="54 USD/pareja"
+        stripe_premium="https://buy.stripe.com/3csbLq6FH5iJ1RSaFW"
+
+       
+        />
+        <Explicacion/>
+        </Layout>
+        
+      </main>
+    </>
+  )
+}
