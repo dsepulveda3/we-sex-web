b756e3d74eca856d6a9473fce50f2255b53d2ffc
diegosepulv <dsepulveda3@uc.cl>
2023-12-21 12:34:34 -0300

    feat(desafio): prices testing new texts

--- pages/premium-material/desafios-para-parejas/discount/index.js
+++ pages/premium-material/desafios-para-parejas/discount/index.js
@@ -7,8 +7,25 @@ import ComoFunciona from '../../../../components/premiumMaterial/desafiosParejas
 import Dudas from '../../../../components/premiumMaterial/dudas'
 import PlansUniversal from '../../../../components/premiumMaterial/desafiosParejas/plansUniversal'
 import Opinions from '../../../../components/premiumMaterial/desafiosParejas/opinions';
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  useEffect(() => {
+    // Check if window is defined (client-side)
+    if (typeof window !== 'undefined') {
+      // This code will only run on the client side
+        const currentRoute = window.location.pathname + window.location.search;
+
+        // Send the complete route to Freshpaint as a custom event
+        if(currentRoute === '/premium-material/desafios-para-parejas/discount'){
+            freshpaint.track("Discount Unique Price", {"Path": currentRoute});
+        }
+        
+    }
+  }, []);
+  
+
   
   return (
     <>
@@ -33,25 +50,30 @@ export default function Home() {
         <Dudas />
         <Opinions />
         <PlansUniversal 
-        discount="Incluye 1er mes gratis"
+        discount="** desde el segundo mes"
 
         suscribirme='Suscribirme (mes gratis)'
         font_boton='1.2rem'
         free_month="yes"
 
-        price_mp_basic="15,600 ARS/pareja"
-        
+        price_mp_basic_discount="AR$ 15,600 mes/pareja"
+        price_mp_basic="AR$ 0 primer mes"
         mp_basic="https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c9380848b62d290018b679bd9b50364" 
-        price_mp_estandar="20,000 ARS/pareja"
+        price_mp_estandar_discount="AR$ 20,000 mes/pareja"
+        price_mp_estandar="AR$ 0 primer mes"
         mp_estandar="https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c9380848b62d285018b679d5eea037c"
-        price_mp_premium="27,000 ARS/pareja"
+        price_mp_premium_discount="AR$ 27,000  mes/pareja"
+        price_mp_premium="AR$ 0 primer mes"
         mp_premium="https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c9380848b62d290018b67a01e2d0369"
 
-        price_stripe_basic="30 USD/pareja"
+        price_stripe_basic_discount="U$D 30 mes/pareja"
+        price_stripe_basic="U$D 0 primer mes"
         stripe_basic="https://buy.stripe.com/8wM8zed45dPf2VW6p6"
-        price_stripe_estandar="38 USD/pareja"
+        price_stripe_estandar_discount="U$D 38 mes/pareja"
+        price_stripe_estandar="U$D 0 primer mes"
         stripe_estandar="https://buy.stripe.com/bIYbLqc0112tcww4gZ"
-        price_stripe_premium="54 USD/pareja"
+        price_stripe_premium_discount="U$D 54 mes/pareja"
+        price_stripe_premium="U$D 0 primer mes"
         stripe_premium="https://buy.stripe.com/aEU8ze8NPeTjeEE4h0"
 
        
