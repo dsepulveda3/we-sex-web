1469313c9ac273ba16b36a9d962e0452ee2b499f
Brian Saavedra <blsaavedra@uc.cl>
2023-12-29 16:30:33 -0300

    test(sw): dont register

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   importScripts: ['/worker.js'],
   // disable: process.env.NODE_ENV === 'development',
-  register: true,
+  // register: true,
   // scope: '/app',
   // sw: 'service-worker.js',
   //...
