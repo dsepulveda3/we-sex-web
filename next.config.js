334112651dec2064632f23b2e6b9720eaad6a250
Brian Saavedra <blsaavedra@uc.cl>
2024-01-08 11:29:17 -0300

    fix(next-pwa): modified config to work with next-pwa 5.5.4

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
 // next.config.js
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  importScripts: ['/worker.js'],
-  // disable: process.env.NODE_ENV === 'development',
-  register: true,
-  scope: '/app',
-  sw: 'service-worker.js',
-  //...
-})
+const withPWA = require("next-pwa");
+
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   importScripts: ['/worker.js'],
+//   // disable: process.env.NODE_ENV === 'development',
+//   register: true,
+//   scope: '/app',
+//   sw: 'service-worker.js',
+//   //...
+// })
 
 module.exports = withPWA({
-  // next.js config
+  pwa: {
+    dest: "public",
+    register: true,
+    scope: '/app',
+    sw: 'service-worker.js',
+  },
 })
