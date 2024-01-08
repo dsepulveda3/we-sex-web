c9b2729a1ca85272bac1b49eaff1d46f3c136906
Brian Saavedra <blsaavedra@uc.cl>
2024-01-08 11:52:36 -0300

    test(web push): register service worker manually

--- components/webPush/index.js
+++ components/webPush/index.js
@@ -93,6 +93,19 @@ const NotificationButton = ({ coupleData, setShowPopup }) => {
     const [isSubscribed, setIsSubscribed] = useState(false)
     const [subscription, setSubscription] = useState(null)
     const [registration, setRegistration] = useState(null)
+
+    useEffect(() => {
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+          .register('/worker.js')
+          .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+          })
+          .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+          });
+      }
+    }, []);
   
     useEffect(() => {
       if (typeof window !== 'undefined' && 'serviceWorker' in navigator && window.workbox !== undefined) {
