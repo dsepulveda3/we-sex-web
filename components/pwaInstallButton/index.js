2fbad4e994c17a18e39c7e1949db8e426de0aede
Brian Saavedra <blsaavedra@uc.cl>
2024-01-29 14:07:51 -0300

    fix(deploy): fixed import

--- components/pwaInstallButton/index.js
+++ components/pwaInstallButton/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
-import AnimatedArrow from './AnimatedArrow';
+import AnimatedArrow from './AnimatedArrow.js';
 
 const PopupContainer = styled.div`
   position: fixed;
